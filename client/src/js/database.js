0d4d4f648624e63c2bee043e2f951234364eb177
TB <tatianabertazoli@gmail.com>
2022-12-22 21:04:27 -0600

    removed return code from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,16 +17,16 @@ export const putDb = async (content) => {
   console.log('Add content to the database', content);
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const textEditorDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the store and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = textEditorDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.put({id:1, value:content});
+  const request = store.put({ value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -37,22 +37,18 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET from the database');
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const textEditorDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the store and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = textEditorDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.get(1);
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result;
+ 
 };
-  
+
 
 initdb();
